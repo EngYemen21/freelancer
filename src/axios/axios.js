5ee93fecaf740c481045ad972dc99f40da2c57b2
779475324 <m.bander.it@gmail.com>
2024-08-27 04:30:57 +0300

    edite axios

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,12 +1,15 @@
 import Axios from 'axios';
-
+// Axios.defaults.withXSRFToken=true;
 const axios = Axios.create({
 	baseURL: "https://freelancer-backend-production-ec21.up.railway.app/api",
-	withCredentials: true,
+	// withCredentials: true,
+	
 	headers: {
 		"Content-Type": "multipart/form-data",
 		"Accept": "application/json",
+		// 'X-XSRF-TOKEN':
 	},
+	withCredentials: true,
 });
 
 export default axios;
